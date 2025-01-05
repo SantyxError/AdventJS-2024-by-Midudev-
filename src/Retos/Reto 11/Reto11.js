f6c2960d6db3e908a2a646a79e000566eb05b0fd
Santiago Benito <bmsanti@hotmail.com>
2025-01-05 17:53:03 +0100

    Reto 11

---
+++ src/Retos/Reto 11/Reto11.js
@@ -0,0 +1,5 @@
+function decodeFilename(filename) {
+  const underscoreIndex = filename.indexOf('_');
+  const lastDotIndex = filename.lastIndexOf('.');
+  return filename.slice(underscoreIndex + 1, lastDotIndex);
+}
