9126ae5d8aefb976f6db1388c965547894697011
Santiago Benito <bmsanti@hotmail.com>
2025-01-13 19:45:57 +0100

    Reto 25

---
+++ src/Retos/Reto 25/Reto25.js
@@ -0,0 +1,23 @@
+function execute(code) {
+  code = [...code];
+  let value = 0;
+  const actions = {
+    ">": () => {},
+    "+": () => value++,
+    "-": () => value--,
+    "[": () => {
+      code = code.slice(code.indexOf("]") - 1);
+    },
+    "]": () => {
+      value = 0;
+    },
+    "{": () => {
+      if (value === 0) code = code.slice(code.indexOf("}"));
+    },
+    "}": () => {},
+  };
+
+  while (code.length) actions[code.shift()]();
+
+  return value;
+}
